a747bee160d233de524208b01445b62a0427eecb
youngjin-korea <youngjin1293@naver.com>
2023-11-23 20:21:06 +0900

    feat[#2]: 배열에 작성한 데이터 저장, list 렌더링 하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
 import DiaryEditor from "./pages/DiaryEditor";
+import DiaryList from "./pages/DiaryList";
 import "./App.css";
+
+const dummyData = [
+  {
+    id: 1,
+    author: "kim",
+    content: "HI",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "kim",
+    content: "HI",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "kim",
+    content: "HI",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 4,
+    author: "kim",
+    content: "HI",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 5,
+    author: "kim",
+    content: "HI",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 6,
+    author: "kim",
+    content: "HI",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyData} />
     </div>
   );
 }
---
+++ src/pages/DiaryItem.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const DiaryItem = ({ author, content, created_date, emotion, id }) => {
+  return (
+    <div className="DiaryItem">
+      <div className="info">
+        <span>
+          작성자 : {author} | 감정점수 : {emotion}
+        </span>
+        <br />
+        <span className="date">{new Date(created_date).toLocaleString()}</span>
+      </div>
+      <div className="content">{content}</div>
+    </div>
+  );
+};
+
+export default DiaryItem;
