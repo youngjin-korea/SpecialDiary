3ebfabff602d9d4dfb26e7b468f336f99115e7eb
youngjin-korea <youngjin1293@naver.com>
2023-11-25 20:12:38 +0900

    feat[#6]: 감정분석 기능 추가, useMemo로 일기 생성 및 삭제 때만 리턴값이 바뀌도록 최적화

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import DiaryEditor from "./pages/DiaryEditor";
 import DiaryList from "./pages/DiaryList";
 import "./App.css";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect, useMemo } from "react";
 
 function App() {
   const [data, setData] = useState([]);
   const dataId = useRef(0);
+
+  const dummy = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/comments")
+      .then((it) => it.json())
+      .catch(console.error("error"));
+
+    const Data = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime,
+        id: dataId.current++,
+      };
+    });
+
+    setData(Data);
+  };
+
+  useEffect(() => {
+    dummy();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -33,9 +55,23 @@ function App() {
     );
   };
 
+  const getDiaryAnalysis = useMemo(() => {
+    console.log("분석실행");
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]);
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 개수: {data.length}</div>
+      <div>기분 좋은 일기 개수: {goodCount}</div>
+      <div>기분 나쁜 일기 개수: {badCount}</div>
+      <div>기분 좋은 일기 비율: {goodRatio}%</div>
       <DiaryList diaryList={data} onDelete={onDelete} onEdit={onEdit} />
     </div>
   );
